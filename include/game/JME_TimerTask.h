942499357408dc6ccbd771cfce9b651182c610b0
Jimmy <329302080@qq.com>
2015-09-14 14:09:18 +0800

    定时任务 命名空间

--- include/game/JME_TimerTask.h
+++ include/game/JME_TimerTask.h
@@ -6,43 +6,50 @@
 
 typedef boost::function<void(boost::shared_ptr<boost::asio::deadline_timer> dt)> JME_TimerTaskHandler;
 
-class JME_TimerTaskCenter
+namespace JMEngine
 {
-public:
-	//************************************
-	// Method:    callbackFromNow
-	// FullName:  JME_TimerTaskCenter::callbackFromNow
-	// Access:    public static 
-	// Returns:   boost::shared_ptr<boost::asio::deadline_timer> --> used to cancel the callback
-	// Qualifier:
-	// Parameter: size_t t ==> task will callback at t sec after now
-	// Parameter: JME_TimerTaskHandler cb
-	//************************************
-	static boost::shared_ptr<boost::asio::deadline_timer> callbackFromNow(size_t t, JME_TimerTaskHandler cb);
+	namespace game
+	{
 
-	//************************************
-	// Method:    callbackAtTime
-	// FullName:  JME_TimerTaskCenter::callbackAtTime
-	// Access:    public static 
-	// Returns:   boost::shared_ptr<boost::asio::deadline_timer>
-	// Qualifier:
-	// Parameter: size_t t ==> task will callback at time t
-	// Parameter: JME_TimerTaskHandler cb
-	//************************************
-	static boost::shared_ptr<boost::asio::deadline_timer> callbackAtTime(size_t t, JME_TimerTaskHandler cb);
+		class JME_TimerTaskCenter
+		{
+		public:
+			//************************************
+			// Method:    callbackFromNow
+			// FullName:  JME_TimerTaskCenter::callbackFromNow
+			// Access:    public static 
+			// Returns:   boost::shared_ptr<boost::asio::deadline_timer> --> used to cancel the callback
+			// Qualifier:
+			// Parameter: size_t t ==> task will callback at t sec after now
+			// Parameter: JME_TimerTaskHandler cb
+			//************************************
+			static boost::shared_ptr<boost::asio::deadline_timer> callbackFromNow(size_t t, JME_TimerTaskHandler cb);
 
-	//************************************
-	// Method:    callbackByInterval
-	// FullName:  JME_TimerTaskCenter::callbackByInterval
-	// Access:    public static 
-	// Returns:   boost::shared_ptr<boost::asio::deadline_timer>
-	// Qualifier:
-	// Parameter: size_t t ==> task will callback 1/t s
-	// Parameter: JME_TimerTaskHandler cb
-	//************************************
-	static boost::shared_ptr<boost::asio::deadline_timer> callbackByInterval(size_t t, JME_TimerTaskHandler cb);
-private:
-	static void timerTaskCallback(const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb);
-	static void cycleTaskCallback(const size_t t, const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb);
-};
+			//************************************
+			// Method:    callbackAtTime
+			// FullName:  JME_TimerTaskCenter::callbackAtTime
+			// Access:    public static 
+			// Returns:   boost::shared_ptr<boost::asio::deadline_timer>
+			// Qualifier:
+			// Parameter: size_t t ==> task will callback at time t
+			// Parameter: JME_TimerTaskHandler cb
+			//************************************
+			static boost::shared_ptr<boost::asio::deadline_timer> callbackAtTime(size_t t, JME_TimerTaskHandler cb);
+
+			//************************************
+			// Method:    callbackByInterval
+			// FullName:  JME_TimerTaskCenter::callbackByInterval
+			// Access:    public static 
+			// Returns:   boost::shared_ptr<boost::asio::deadline_timer>
+			// Qualifier:
+			// Parameter: size_t t ==> task will callback 1/t s
+			// Parameter: JME_TimerTaskHandler cb
+			//************************************
+			static boost::shared_ptr<boost::asio::deadline_timer> callbackByInterval(size_t t, JME_TimerTaskHandler cb);
+		private:
+			static void timerTaskCallback(const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb);
+			static void cycleTaskCallback(const size_t t, const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb);
+		};
+	}
+}
 #endif // JME_TimerTask_h__
--- source/game/JME_TimerTask.cpp
+++ source/game/JME_TimerTask.cpp
@@ -3,52 +3,59 @@
 
 #include "boost/make_shared.hpp"
 
-boost::shared_ptr<boost::asio::deadline_timer> JME_TimerTaskCenter::callbackFromNow(size_t t, JME_TimerTaskHandler cb)
+namespace JMEngine
 {
-	auto dt = boost::make_shared<boost::asio::deadline_timer>(JMECore.getLogicioService());
-	dt->expires_from_now(boost::posix_time::seconds(t));
-	dt->async_wait(boost::bind(JME_TimerTaskCenter::timerTaskCallback, boost::asio::placeholders::error, dt, cb));
-	return dt;
-}
-
-void JME_TimerTaskCenter::timerTaskCallback(const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb)
-{
-	if (!err)
+	namespace game
 	{
-		cb(dt);
-	}
-	else
-	{
-		//该任务被取消
-	}
-}
 
-boost::shared_ptr<boost::asio::deadline_timer> JME_TimerTaskCenter::callbackAtTime(size_t t, JME_TimerTaskHandler cb)
-{
-	auto dt = boost::make_shared<boost::asio::deadline_timer>(JMECore.getLogicioService(), boost::posix_time::from_time_t(t));
-	dt->async_wait(boost::bind(JME_TimerTaskCenter::timerTaskCallback, boost::asio::placeholders::error, dt, cb));
-	return dt;
-}
+		boost::shared_ptr<boost::asio::deadline_timer> JME_TimerTaskCenter::callbackFromNow(size_t t, JME_TimerTaskHandler cb)
+		{
+			auto dt = boost::make_shared<boost::asio::deadline_timer>(JMECore.getLogicioService());
+			dt->expires_from_now(boost::posix_time::seconds(t));
+			dt->async_wait(boost::bind(JME_TimerTaskCenter::timerTaskCallback, boost::asio::placeholders::error, dt, cb));
+			return dt;
+		}
 
-boost::shared_ptr<boost::asio::deadline_timer> JME_TimerTaskCenter::callbackByInterval(size_t t, JME_TimerTaskHandler cb)
-{
-	auto dt = boost::make_shared<boost::asio::deadline_timer>(JMECore.getLogicioService());
-	dt->expires_from_now(boost::posix_time::seconds(t));
-	dt->async_wait(boost::bind(JME_TimerTaskCenter::cycleTaskCallback, t, boost::asio::placeholders::error, dt, cb));
-		
-	return dt;
-}
+		void JME_TimerTaskCenter::timerTaskCallback(const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb)
+		{
+			if (!err)
+			{
+				cb(dt);
+			}
+			else
+			{
+				//该任务被取消
+			}
+		}
 
-void JME_TimerTaskCenter::cycleTaskCallback(const size_t t, const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb)
-{
-	if (!err)
-	{
-		cb(dt);
-		dt->expires_from_now(boost::posix_time::seconds(t));
-		dt->async_wait(boost::bind(JME_TimerTaskCenter::cycleTaskCallback, t, boost::asio::placeholders::error, dt, cb));
-	}
-	else
-	{
-		//该任务被取消
+		boost::shared_ptr<boost::asio::deadline_timer> JME_TimerTaskCenter::callbackAtTime(size_t t, JME_TimerTaskHandler cb)
+		{
+			auto dt = boost::make_shared<boost::asio::deadline_timer>(JMECore.getLogicioService(), boost::posix_time::from_time_t(t));
+			dt->async_wait(boost::bind(JME_TimerTaskCenter::timerTaskCallback, boost::asio::placeholders::error, dt, cb));
+			return dt;
+		}
+
+		boost::shared_ptr<boost::asio::deadline_timer> JME_TimerTaskCenter::callbackByInterval(size_t t, JME_TimerTaskHandler cb)
+		{
+			auto dt = boost::make_shared<boost::asio::deadline_timer>(JMECore.getLogicioService());
+			dt->expires_from_now(boost::posix_time::seconds(t));
+			dt->async_wait(boost::bind(JME_TimerTaskCenter::cycleTaskCallback, t, boost::asio::placeholders::error, dt, cb));
+
+			return dt;
+		}
+
+		void JME_TimerTaskCenter::cycleTaskCallback(const size_t t, const boost::system::error_code& err, boost::shared_ptr<boost::asio::deadline_timer> dt, JME_TimerTaskHandler cb)
+		{
+			if (!err)
+			{
+				cb(dt);
+				dt->expires_from_now(boost::posix_time::seconds(t));
+				dt->async_wait(boost::bind(JME_TimerTaskCenter::cycleTaskCallback, t, boost::asio::placeholders::error, dt, cb));
+			}
+			else
+			{
+				//该任务被取消
+			}
+		}
 	}
 }
