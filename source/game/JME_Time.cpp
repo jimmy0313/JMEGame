6b6f590046cf107dd0e03d6caea1206183b50535
Jimmy <329302080@qq.com>
2015-09-14 14:11:02 +0800

    新增一个 时间类 简单获取当前时间

---
+++ source/game/JME_Time.cpp
@@ -0,0 +1,34 @@
+#include "JME_Time.h"
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+namespace JMEngine
+{
+	namespace game
+	{
+
+		bool JME_Time::isTimeToday(time_t t)
+		{
+			auto ct = boost::posix_time::from_time_t(t);
+			auto nt = boost::posix_time::second_clock::local_time();
+
+			return ct.date() == nt.date();
+		}
+
+		int JME_Time::todayYearday()
+		{
+			auto nt = boost::posix_time::second_clock::local_time();
+			return nt.date().day_of_year();
+		}
+
+		tm JME_Time::localTime()
+		{
+			return boost::posix_time::to_tm(boost::posix_time::second_clock::local_time());
+		}
+
+		tm JME_Time::localTime(time_t t)
+		{
+			return boost::posix_time::to_tm(boost::posix_time::from_time_t(t));
+		}
+
+	}
+}
