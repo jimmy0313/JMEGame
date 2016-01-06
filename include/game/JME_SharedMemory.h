7da024b87f1233b2040b86a18073cb91cbe6241e
Jimmy <329302080@qq.com>
2016-01-06 12:03:55 +0800

    共享内存模板 优化

--- include/game/JME_SharedMemory.h
+++ include/game/JME_SharedMemory.h
@@ -28,7 +28,10 @@ namespace JMEngine
 		public:
 			typedef map<string, boost::interprocess::mapped_region*> MappedRegion;
 
+			//主要用于cache, 用于负责cache的进程 创建共享内存 设置正确数据(如 从内存读取)
 			static boost::shared_ptr<T> create(boost::interprocess::create_only_t, const char* name, boost::interprocess::mode_t mode);
+
+			//主要用于logic，用于logic进程， 对数据进行修改
 			static boost::shared_ptr<T> create(boost::interprocess::open_only_t, const char* name, boost::interprocess::mode_t mode);
 
 		private:
@@ -86,13 +89,22 @@ namespace JMEngine
 		{
 			try
 			{
+				//移除可能存在老共享内存
+				JMEngine::game::JME_SharedMemory<T>::deleteMappedRegion(name);
+				boost::interprocess::shared_memory_object::remove(name);
+
+				//创建新的共享内存, 并分配大小
 				boost::interprocess::shared_memory_object shm(boost::interprocess::create_only, name, mode);
 				shm.truncate(sizeof(T));
 
+				//创建内存映射
 				auto mmap = new boost::interprocess::mapped_region(shm, mode);
+
+				//保存内存映射信息
 				JMEngine::game::JME_SharedMemory<T>::saveMappedRegion(name, mmap);
 
-				//创建共享内存的才能去销毁共享内存
+				//通过共享内存 创建shared_ptr<T>对象
+				//共享内存， 谁创建， 谁销毁
 				auto ptr = boost::shared_ptr<T>(new(mmap->get_address()) T, boost::bind(JME_SharedMemory<T>::destory, name, true));
 				return ptr;
 			}
@@ -109,24 +121,29 @@ namespace JMEngine
 		{
 			try
 			{
-
+				//检测当前进程是否已经打开共享内存
 				if (JMEngine::game::JME_SharedMemory<T>::existMappedRegion(name))
 				{
 					LOGE("The shared memory with name [ %s ] had been opened", name);
 					return boost::shared_ptr<T>();
 				}
+				//打开共享内存
 				boost::interprocess::shared_memory_object shm(boost::interprocess::open_only, name, mode);
 
+				//创建内存映射
 				auto mmap = new boost::interprocess::mapped_region(shm, mode);
+
+				//如何共享内存分配内存大小 与 对象大小不一致， 对象被修改（大小一致 并不代表 对象没有被修改）
 				if (sizeof(T) != mmap->get_size())
 				{
+					//移除内存映射
 					delete mmap;
-					boost::interprocess::shared_memory_object::remove(name);
 					return boost::shared_ptr<T>();
 				}
+				//保存内存映射
 				JMEngine::game::JME_SharedMemory<T>::saveMappedRegion(name, mmap);
 
-				//打开共享内存的 无法销毁共享内存 只能移除在本进程中的映射
+				//直接将共享内存 转为对象， 不再调用构造函数
 				auto ptr = boost::shared_ptr<T>((T*)mmap->get_address(), boost::bind(JME_SharedMemory<T>::destory, name, false));
 				return ptr;
 			}
